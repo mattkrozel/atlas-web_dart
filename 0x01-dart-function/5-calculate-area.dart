2a15817d5e38addfd57daa609e5f7a7f063037aa
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-10-08 13:12:24 -0500

    task 5 †riangle area

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,3 @@
+double calculateArea(double height, double base) {
+  return ((.5 * base * height * 100).round() / 100);
+}
---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,3 @@
+List<double> convertToF(List<double> temperaturesInC) {
+  
+}
