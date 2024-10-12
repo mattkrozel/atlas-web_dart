29eae8da1e9144722ee1ddfab4f23cc996d6deb0
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-10-11 21:27:54 -0500

    pushed new file and duplicated last

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,18 @@
+class Password {
+  String? _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (_password == '') return false;
+    if (_password!.length < 8 || _password!.length > 16) return false;
+    if ((!_password!.contains(new RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])')))) return false; //|| (!password.contains(new RegExp(r'[a-z]'))) || (!password.contains(new RegExp(r'[0-9]')))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Your Password is: ${_password}';
+  }
+}
