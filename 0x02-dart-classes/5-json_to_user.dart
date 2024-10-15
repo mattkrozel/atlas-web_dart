c622f9bd338986ad15611c29431f49968fbc9112
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-10-15 12:24:41 -0500

    task 5 done

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,29 @@
+class User {
+  User({required this.id, required this.name, required this.age, required this.height});
+  int id;
+  String name;
+  int age;
+  double height;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return new User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height']
+    );
+  }
+
+  String toString() {
+    return 'User(id: $id ,name: $name, age: $age, height: $height)';
+  }
+}
